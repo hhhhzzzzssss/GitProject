fd5848e38130e19258ecfa4b1c673cde86cc7a83
HuangGong <Hzs144207>
2023-06-13 23:06:05 +0800

    third commit

--- src/main/java/com/project/main.java
+++ src/main/java/com/project/main.java
@@ -3,6 +3,7 @@ package com.project;
 public class main {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("test modify");
+        System.out.println("second modify");
+        System.out.println("third modify");
     }
 }
