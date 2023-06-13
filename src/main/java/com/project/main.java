b1e054b61d93e5df4fb06612bb09e19dfddd8856
HuangGong <Hzs144207>
2023-06-13 22:51:13 +0800

    second commit

--- src/main/java/com/project/main.java
+++ src/main/java/com/project/main.java
@@ -3,5 +3,6 @@ package com.project;
 public class main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("test modify");
     }
 }
